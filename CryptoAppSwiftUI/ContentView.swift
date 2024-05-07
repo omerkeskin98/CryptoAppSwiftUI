bf9d0dd1a04e425b76a97697996d0c8f1a16f5a6
Omer Faruk Keskin <keskinomerf@gmail.com>
2024-05-07 15:36:41 +0300

    Initial Commit

---
+++ CryptoAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Omer Keskin on 7.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoAppSwiftUI/CryptoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppSwiftUIApp.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Omer Keskin on 7.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
